47ba1df19cce398ee1d7509c4052ffb48e3bfc01
dommymogeni <69898848+dommymogeni@users.noreply.github.com>
2020-09-14 17:29:30 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include("header.php");?>
+<section id="page"><font color="#FFFFFF"></font>
+<header id="pageheader" class="homeheader">
+<h1 class="sitedescription"><h2>&nbsp;</h2></h1>
+</header>
+
+
+<article class="post"></article>
+
+<article class="post">
+<header class="postheader">
+<h1>Student Information System Supports:</h1><p class="postinfo">&nbsp;</p>
+</header>
+<p><b>•	Handling inquiries from prospective students.</b></p>
+<p><b>•	Handling the Student details.  </b></p>
+<p><b>•	Maintaining the Student Marks Details.  </b></p>
+<p><b>•	Handling Student Attendance Records.  </b></p>
+<p><b>•	Maintaining records of absences and attendance.  </b></p>
+<p><b>•	Maintaining records of Internal marks.</b></p>
+</article>
+<div class="clear"></div>
+
+<div class="clear"></div>
+</section>
+</div>
+<?php include("footer.php"); ?>
